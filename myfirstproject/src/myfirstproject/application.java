36ef7f6055d6cf12724ce20aa5d9982e79abb5c6
Dominic-Atari <kikosimikehop2@gmail.com>
2024-12-05 11:24:45 -0600

    Add files via upload

---
+++ myfirstproject/src/myfirstproject/application.java
@@ -0,0 +1,21 @@
+package myfirstproject;
+
+public class application {
+
+	public static void main(String[] args) {
+		//find the area of a Trapezium.
+		int shorterSide = 5;
+		int longerSide = 10;
+		int hight = 7;
+		
+		// sum of a and b.
+		int totalLength = shorterSide + longerSide;
+		System.out.println(totalLength);
+		
+		// a+b * h.
+		int lengthHight = totalLength * hight;
+		
+		//are 
+		System.out.println(0.5 * lengthHight);
+	}
+}
